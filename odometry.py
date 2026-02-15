a311f6ca2fff4337ee27af9e089aa981b33a0821
heisenberg5869 <vtsarrveshwar@gmail.com>
2026-02-15 19:04:29 +0530

    Corrected Odometry

--- odometry.py
+++ odometry.py
@@ -8,12 +8,12 @@ class Odometry:
         self.gt_x = 5.0
         self.gt_y = 2.5
         self.gt_theta = 0.0
-        self.conversion_factor = 1.3
+        self.conversion_factor = 1.0 # Removed the 1.3 conversion_factor scaling
 
         # Estimated pose (odometry)
         self.x = 5.0
         self.y = 2.5
-        self.theta = 0.523 #(pi/6)
+        self.theta = 0.0  # Removed the initial bias pi/6
 
     def update(self, v, omega, dt):
         """
@@ -21,17 +21,25 @@ class Odometry:
         using commanded linear and angular velocity.
         """
 
-        # --------------------------------
+        #--------------------------------
         # Ground truth motion integration
-        # --------------------------------
+        #--------------------------------
         self.gt_x += v * math.cos(self.gt_theta) * dt
         self.gt_y += v * math.sin(self.gt_theta) * dt
         self.gt_theta += omega * dt
         self.gt_theta = normalize_angle(self.gt_theta)
 
-        # --------------------------------
+        #--------------------------------
         # Odometry motion integration
-        # --------------------------------
-        self.x += v * math.sin(self.theta + 0.1) * dt
-        self.y += v * math.cos(self.theta) * dt
+        #--------------------------------
+        # Interchanged cos and sin
+        self.x += v * math.cos(self.theta) * dt # Removed the +0.1 noise offset
+        self.y += v * math.sin(self.theta) * dt
         self.theta += omega * dt *self.conversion_factor
+        self.theta = normalize_angle(self.theta) # Added normalization to keep theta in [-pi, pi]
+
+
+
+ 
+
+  
